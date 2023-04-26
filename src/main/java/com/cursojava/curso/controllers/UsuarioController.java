c1ff005eadebe65bb75463f36062c59419ced19d
kevoaac <130163890+kevoaac@users.noreply.github.com>
2023-04-26 13:24:58 -0500

    update proyect creacion de servicio REST con un modelo Usuario y controladores

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,14 +1,60 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Constructor;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "prueba")
-    public List<String> prueba(){
-        return List.of("Manzana", "Kiwi", "Banana");
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsurio(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setTelefono("09999999");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setPassword("123456789");
+        return usuario;
     }
+
+    @RequestMapping(value = "usuarioo")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setTelefono("09999999");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setPassword("123456789");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuariooo")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setTelefono("09999999");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setPassword("123456789");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuarioooo")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setTelefono("09999999");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setPassword("123456789");
+        return usuario;
+    }
+
 }
+
+
