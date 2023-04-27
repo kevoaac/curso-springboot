d1b5b07b58a82a38577cefd4d5f45db041b3d3a2
kevoaac <130163890+kevoaac@users.noreply.github.com>
2023-04-26 23:34:34 -0500

    insert users into table using json request and AJAX
    Realizamos peticiones desde el servidor(cuando se cargue la página http://localhost:8080/usuarios.html),
    para que me inserte dentro de la tabla, un html personalizado para cada usuario de una lista de usuarios (AJAX),
    Llamar a una API con AJAX

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -6,20 +6,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "usuario/{id}")
-    public Usuario getUsurio(@PathVariable Long id){
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsurios(){
+        List<Usuario> usuarios = new ArrayList<>();
+
         Usuario usuario = new Usuario();
-        usuario.setId(id);
+        usuario.setId(123L);
         usuario.setNombre("Lucas");
         usuario.setApellido("Moy");
         usuario.setTelefono("09999999");
         usuario.setEmail("lucasmoy@gmail.com");
         usuario.setPassword("123456789");
-        return usuario;
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(234L);
+        usuario2.setNombre("Jose");
+        usuario2.setApellido("Luna");
+        usuario2.setTelefono("09999999");
+        usuario2.setEmail("joslu@gmail.com");
+        usuario2.setPassword("123456789");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(456L);
+        usuario3.setNombre("Martha");
+        usuario3.setApellido("Castro");
+        usuario3.setTelefono("09999999");
+        usuario3.setEmail("marcastro@gmail.com");
+        usuario3.setPassword("123456789");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        return usuarios;
     }
 
     @RequestMapping(value = "usuarioo")
---
+++ src/main/resources/static/js/usuarios.js
@@ -0,0 +1,38 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() { //Cuando la página cargue llama a esta función
+
+  cargarUsuarios(); //llamo a mi función creada
+  $('#usuarios').DataTable();
+});
+
+async function cargarUsuarios(){//Agregamos async por awaitde abajo
+    //dentro agregaremos toda nuestra lógica
+      const request = await fetch('usuarios', {
+        method: 'GET', //await: lo que hace es esperar el resultado del llamado
+        headers: {//Indica que usará json y que el contenido es json
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      });
+      const usuarios = await request.json();
+
+
+
+
+
+
+
+      let listadoHtml = '';//lista a guardar string de html de los usuarios
+      //for each
+      for(let usuario of usuarios){
+        let usuarioHtml = ' <tr><td>'
+        +usuario.id +'</td><td>'
+        +usuario.nombre+' '+usuario.apellido+'</td><td>'
+        +usuario.email+'</td><td>'
+        +usuario.telefono+'</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
+
+        listadoHtml = listadoHtml + usuarioHtml; //concatenamos html de cada usuario
+      }
+
+      document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
+}
